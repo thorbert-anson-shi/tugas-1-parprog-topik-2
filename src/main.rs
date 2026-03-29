19d839089deddda564cf508a526d1aade6933223
Alpha SM <alpha.sutha@ui.ac.id>
2026-03-29 08:40:57 +0700

    ambil avg aja

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use humantime::format_duration;
 use std::env;
 use std::process::Command;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 fn run(src: &str) -> std::time::Duration {
     let start = Instant::now();
@@ -15,16 +15,30 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 || args.len() > 3 {
-        eprintln!("usage: {} <file1.c> [file2.c]", args[0]);
+        eprintln!("usage: {} <file1> [file2]", args[0]);
         std::process::exit(1);
     }
 
-    let d1 = run(&args[1]);
+    let n = 20;
+
+    // file 1
+    let _ = run(&args[1]);
+    let mut total1 = Duration::ZERO;
+    for _ in 0..n {
+        total1 += run(&args[1]);
+    }
+    let d1 = total1 / n as u32;
 
     if args.len() == 2 {
         println!("{}", format_duration(d1));
     } else {
-        let d2 = run(&args[2]);
+        let _ = run(&args[2]);
+        let mut total2 = Duration::ZERO;
+        for _ in 0..n {
+            total2 += run(&args[2]);
+        }
+        let d2 = total2 / n as u32;
+
         println!("{} | {}", format_duration(d1), format_duration(d2));
     }
 }
