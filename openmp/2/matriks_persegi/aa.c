e69b7c803a350b79a481a7a47dae60d4b16f63db
alpha <alpha.sutha@ui.ac.id>
2026-04-03 19:58:48 +0700

    memory tunning

--- openmp/2/matriks_persegi/aa.c
+++ openmp/2/matriks_persegi/aa.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// lebih baik contigus di operasi READ Sr. ChatGPT
+void transpose(double *B, double *B_T, int N) {
+#pragma omp parallel for schedule(static)
+  for (int j = 0; j < N; j++) {
+#pragma omp simd
+    for (int i = 0; i < N; i++) {
+      B_T[j * N + i] = B[i * N + j];
+    }
+  }
+}
+
 void matmul_avx2(double *A, double *B, double *C, int N) {
 #pragma omp parallel for schedule(static)
   for (int i = 0; i < N; i++) {
@@ -9,7 +20,7 @@ void matmul_avx2(double *A, double *B, double *C, int N) {
       double sum = 0.0;
 #pragma omp simd reduction(+ : sum)
       for (int k = 0; k < N; k++) {
-        sum += A[i * N + k] * B[k * N + j];
+        sum += A[i * N + k] * B[j * N + k];
       }
       C[i * N + j] = sum;
     }
@@ -20,6 +31,7 @@ int main() {
   int N = 4096;
   double *A = aligned_alloc(32, N * N * sizeof(double));
   double *B = aligned_alloc(32, N * N * sizeof(double));
+  double *B_T = aligned_alloc(32, N * N * sizeof(double));
   double *C = aligned_alloc(32, N * N * sizeof(double));
 
   for (int i = 0; i < N * N; i++)
@@ -34,7 +46,8 @@ int main() {
 
   for (int r = 0; r < repeat; r++) {
     double t1 = omp_get_wtime();
-    matmul_avx2(A, B, C, N);
+    transpose(B, B_T, N);
+    matmul_avx2(A, B_T, C, N);
     double t2 = omp_get_wtime();
     total_time += (t2 - t1);
   }
@@ -44,6 +57,7 @@ int main() {
 
   free(A);
   free(B);
+  free(B_T);
   free(C);
   return 0;
 }
